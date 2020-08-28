05765bb15ef81cd6ec6f08e9122ba72795dacb6a
VianneyMI <vianney.mixtur@outlook.fr>
2020-08-28 19:22:32 +0200

    Optimized backend structure

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
 
-const Thing = require('./models/thing');
 const app = express();
 
 mongoose.connect('mongodb+srv://vianmixt:StrongPa55word@cluster0.gukh8.mongodb.net/test?retryWrites=true&w=majority')
@@ -16,8 +16,6 @@ mongoose.connect('mongodb+srv://vianmixt:StrongPa55word@cluster0.gukh8.mongodb.n
     console.error(error);
   })
 
-app.use(bodyParser.json());
-
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -26,97 +24,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/stuff', (req, res, next) => {
-  const thing = new Thing({
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  thing.save().then(
-    ()=>{
-      res.status(201).json({
-        message: 'Post saved succesfully!'
-      })
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+app.use(bodyParser.json());
 
-app.get('/api/stuff/:id', (req, res, next) => {
-  Thing.findOne({
-    _id: req.params.id
-  }).then(
-    (thing) => {
-      res.status(200).json(thing);
-    }
-  ).catch(
-    (error) => {
-      res.status(404).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  const thing = new Thing({
-    _id: req.params.id,
-    title: req.body.title,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl,
-    price: req.body.price,
-    userId: req.body.userId
-  });
-  Thing.updateOne({_id: req.params.id}, thing).then(
-    () => {
-      res.status(201).json({
-        message: 'Thing updated successfully!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
-
-app.use('/api/stuff', (req, res, next) => {
-  Thing.find().then(
-    (things) => {
-      res.status(200).json(things);
-    }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports=app;
