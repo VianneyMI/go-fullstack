0e230125d88d8dcbf3e02815d9c31b0d0b122b03
VianneyMI <vianney.mixtur@outlook.fr>
2020-08-28 17:42:05 +0200

    Connected to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,22 @@
+//MONGODB CONNECTION mongodb+srv://vianmixt:StrongPa55word!@cluster0.gukh8.mongodb.net/<dbname>?retryWrites=true&w=majority
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
 const app = express();
+
+mongoose.connect('mongodb+srv://vianmixt:StrongPa55word@cluster0.gukh8.mongodb.net/test?retryWrites=true&w=majority')
+  .then(()=>{
+    console.log('Succesfully connected to MongoDB Atlas!');
+  })
+  .catch((error)=>{
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  })
+
+app.use(bodyParser.json());
 
 
 app.use((req, res, next) => {
@@ -10,36 +26,97 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
-
-
-app.post('api/stuff', (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message:'Success'
+app.post('/api/stuff', (req, res, next) => {
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
   });
+  thing.save().then(
+    ()=>{
+      res.status(201).json({
+        message: 'Post saved succesfully!'
+      })
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 
-app.use('/api/stuff', (req, res, next) => {
-      const stuff = [
-        {
-          _id: 'oeihfzeoi',
-          title: 'My first thing',
-          description: 'All of the info about my first thing',
-          imageUrl: '',
-          price: 4900,
-          userId: 'qsomihvqios',
-        },
-        {
-          _id: 'oeihfzeomoihi',
-          title: 'My second thing',
-          description: 'All of the info about my second thing',
-          imageUrl: '',
-          price: 3900,
-          userId: 'qsomihvqios',
-        },
-      ];
-      res.status(200).json(stuff);
-    });
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({
+    _id: req.params.id
+  }).then(
+    (thing) => {
+      res.status(200).json(thing);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  const thing = new Thing({
+    _id: req.params.id,
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  Thing.updateOne({_id: req.params.id}, thing).then(
+    () => {
+      res.status(201).json({
+        message: 'Thing updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find().then(
+    (things) => {
+      res.status(200).json(things);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+});
 
 module.exports=app;
